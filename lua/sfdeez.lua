9dd166985fe96758d63d2777053fd1cee61a86fe
devinjc213 <devinjc213@gmail.com>
2023-11-13 21:10:29 -0600

    add testing commands, optional args

--- lua/sfdeez.lua
+++ lua/sfdeez.lua
@@ -1,8 +1,16 @@
 local function auth(alias)
+  if not alias or alias == '' then
+    alias = vim.fn.input('Alias: ')
+  end
+
   local job = vim.fn.jobstart('sfdx force:auth:web:login -a ' .. alias)
 end
 
 local function create_class(name)
+  if not name or name == '' then
+    name = vim.fn.input('Class Name: ')
+  end
+
   local job = vim.fn.jobstart('sfdx force:apex:class:create -n ' .. name, {
     on_exit = function(_, code)
       if code == 0 then
@@ -13,6 +21,10 @@ local function create_class(name)
 end
 
 local function create_trigger(name)
+  if not name or name == '' then
+    name = vim.fn.input('Trigger Name: ')
+  end
+
   local job = vim.fn.jobstart('sfdx force:apex:trigger:create -n ' .. name, {
     on_exit = function(_, code)
       if code == 0 then
@@ -26,6 +38,47 @@ local function deploy()
   local current_file = vim.fn.expand('%:p')
   local cmd = 'sfdx force:source:deploy -p ' .. current_file
   vim.cmd('! ' .. cmd)
+end
+
+local function run_test_file()
+  local current_file = vim.fn.expand('%:p')
+  local cmd = 'sfdx force:apex:test:run -l RunLocalTests -r human -w 10 -c -d ' .. current_file
+
+  vim.cmd('hsplit')
+
+  local new_buffer = vim.fn.bufnr('%')
+
+  local job = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      vim.api.nvim_buf_set_lines(new_buffer, 0, -1, false, vim.split(data, '\n'))
+    end,
+    on_exit = function(_, _)
+      vim.cmd('normal! ggVGd')
+    end,
+  })
+
+  vim.cmd('wincmd w')
+end
+
+local function run_test_at_cursor()
+  local current_file = vim.fn.expand('%:p')
+  local current_line = vim.fn.line('.')
+  local cmd = 'sfdx force:apex:test:run -l RunLocalTests -r human -w 10 -c -d ' .. current_file .. ' -n ' .. current_line
+
+  vim.cmd('hsplit')
+
+  local new_buffer = vim.fn.bufnr('%')
+
+  local job = vim.fn.jobstart(cmd, {
+    on_stdout = function(_, data, _)
+      vim.api.nvim_buf_set_lines(new_buffer, 0, -1, false, vim.split(data, '\n'))
+    end,
+    on_exit = function(_, _)
+      vim.cmd('normal! ggVGd')
+    end,
+  })
+
+  vim.cmd('wincmd w')
 end
 
 return {
--- plugin/sfdeez.vim
+++ plugin/sfdeez.vim
@@ -6,6 +6,8 @@ set cpo&vim
 command! -nargs=1 SFDXAuth lua require'sfdeez'.auth(<f-args>)
 command! -nargs=1 SFDXCreateClass lua require'sfdeez'.create_class(<f-args>)
 command! -nargs=1 SFDXCreateTrigger lua require'sfdeez'.create_trigger(<f-args>)
+command! -nargs=0 SFDXRunTestFile lua require'sfdeez'.run_test_file()
+command! -nargs=0 SFDXRunTestAtCursor lua require 'sfdeez'.run_test_at_cursor()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
