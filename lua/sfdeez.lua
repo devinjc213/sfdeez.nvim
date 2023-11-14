6c601890b861fb14d9436d64f20c4ed844b96d76
devinjc213 <devinjc213@gmail.com>
2023-11-13 20:11:34 -0600

    add return

--- lua/sfdeez.lua
+++ lua/sfdeez.lua
@@ -1,8 +1,8 @@
-function auth(alias)
+local function auth(alias)
   local job = vim.fn.jobstart('sfdx force:auth:web:login -a ' .. alias, {})
 end
 
-function create_class(name)
+local function create_class(name)
   local job = vim.fn.jobstart('sfdx force:apex:class:create -n ' .. name, {
     on_exit = function(_, code)
       if code == 0 then
@@ -12,7 +12,7 @@ function create_class(name)
   })
 end
 
-function create_trigger(name)
+local function create_trigger(name)
   local job = vim.fn.jobstart('sfdx force:apex:trigger:create -n ' .. name, {
     on_exit = function(_, code)
       if code == 0 then
@@ -22,8 +22,15 @@ function create_trigger(name)
   })
 end
 
-function deploy()
+local function deploy()
   local current_file = vim.fn.expand('%:p')
   local cmd = 'sfdx force:source:deploy -p ' .. current_file
   vim.cmd('! ' .. cmd)
 end
+
+return {
+  auth = auth,
+  create_class = create_class,
+  create_trigger = create_trigger,
+  deploy = deploy
+}
